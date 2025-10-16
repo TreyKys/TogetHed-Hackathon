64bbe1bf769dc136edf02699d5992691d14aadf6
TreyKys <ogunlanadayo01187@gmail.com>
2025-10-16 01:09:17 +0100

    Update hedera.js

--- skillswap-react/src/hedera.js
+++ skillswap-react/src/hedera.js
@@ -1,6 +1,6 @@
 // We only need the contract addresses now.
 // The transaction building will happen directly in App.jsx.
 
-export const escrowContractAddress =  0xaB88Af4647228099fBd2904C25A6feb657beb19a ;
+export const escrowContractAddress = "0xaB88Af4647228099fBd2904C25A6feb657beb19a";
 
 export const assetTokenContractAddress = " 0x5458d6964b3ED0C83Bb7d96AFE927D813A5DDC0D ";
